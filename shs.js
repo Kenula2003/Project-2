9125f6eb683197528462d25f674190a20df3e756
Kenula2003 <slpubg75@gmail.com>
2025-01-06 18:44:20 +0100

    hello

---
+++ shs.js
@@ -0,0 +1,8 @@
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+console.log(yearsCompleted == firstYearCompleted)
+console.log(yearsCompleted === firstYearComplete)
+
+console.logg("hello how are you")
+const hello = 10
